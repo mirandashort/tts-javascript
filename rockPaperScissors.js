256d2db2f73132a551a187ee472ee34132ac90af
Miranda Short <mird2794@gmail.com>
2018-12-21 10:19:46 -0500

    roll into just one function

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -9,21 +9,18 @@ document.addEventListener('DOMContentLoaded', function() {
   var turn = []
 
   var randomPossibility = possibilities[Math.floor(Math.random() * possibilities.length)];
-  var readComputer = function(possibilities) {
+  var eval = function(possibilities) {
     document.getElementById('read-computer').innerHTML = 'Computer played ' + randomPossibility;
-    result = randomPossibility;
-    turn.push({computer: result});
-  };
 
-  var readPlayer = function() {
     var playerSelection = document.getElementById('player-choice');
     var selectedValue = playerSelection.options[playerSelection.selectedIndex].value;
-    var textValue = playerSelection.options[playerSelection.selectedIndex].text;
-    document.getElementById('read-player').innerHTML = 'You played ' + textValue
-    return textValue;
-  }
+    document.getElementById('read-player').innerHTML = 'You played ' + selectedValue
+
+    turn.push([{computer: randomPossibility}, {player: selectedValue}]);
+    console.log(turn)
+  };
+
   var challengeComputer = document.getElementById("challenge-computer")
-  challengeComputer.addEventListener("click", readPlayer);
-  challengeComputer.addEventListener("click", readComputer);
+  challengeComputer.addEventListener("click", eval);
 
 });

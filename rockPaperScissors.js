0cb1f1affb04154678167b8154e0b4da9d60f12b
Miranda Short <mird2794@gmail.com>
2018-12-23 08:17:36 -0500

    finito

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -6,10 +6,11 @@ document.addEventListener('DOMContentLoaded', function() {
     'paper',
     'scissors'
   ];
-  var turn = []
 
-  var randomPossibility = possibilities[Math.floor(Math.random() * possibilities.length)];
-  var eval = function(possibilities) {
+  var turn = [];
+
+  var eval = function() {
+    var randomPossibility = possibilities[Math.floor(Math.random() * possibilities.length)];
     document.getElementById('read-computer').innerHTML = 'Computer played ' + randomPossibility;
 
     var playerSelection = document.getElementById('player-choice');
@@ -17,10 +18,40 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('read-player').innerHTML = 'You played ' + selectedValue
 
     turn.push([{computer: randomPossibility}, {player: selectedValue}]);
-    console.log(turn)
   };
 
-  var challengeComputer = document.getElementById("challenge-computer")
-  challengeComputer.addEventListener("click", eval);
+  var computerScore = 0
+  var playerScore = 0
+
+  var getCurrentScore = function() {
+    var computerPlay = turn[0][0].computer
+    var playerPlay = turn[0][1].player
+
+    if ((computerPlay === 'rock' && playerPlay === 'scissors') || (computerPlay === 'paper' && playerPlay === 'rock') || (computerPlay === 'scissors' && playerPlay === 'paper')) {
+      computerScore += 1
+    } else if ((computerPlay === 'scissors' && playerPlay === 'rock') || (computerPlay === 'rock' && playerPlay === 'paper') || (computerPlay === 'paper' && playerPlay === 'scissors')) {
+      playerScore += 1
+    }
+    else computerScore += 0
+    turn.shift()
+
+    document.getElementById('current-score').innerHTML =
+      'Computer: ' + computerScore + ', ' + 'You: ' + playerScore
+
+    if (computerScore === 3) {
+      alert('Computer wins!')
+    }
+    else if (playerScore === 3) {
+      alert('You win!')
+    }
+    else {
+      alert('Computer: ' + computerScore + ', ' + 'You: ' + playerScore)
+    }
+  }
+
+
+  document.getElementById("challenge-computer").addEventListener("click", eval);
+  document.getElementById('get-current-score').addEventListener("click", getCurrentScore)
+
 
 });
